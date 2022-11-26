b8cb6837b05aaf3f9ea758f8a64dc74023b65a16
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2022-11-26 17:39:47 +0530

    new

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const QuestionRoute = require("./routes/Question.route");
+const QuestionRoute = require("./src/routes/Question.route");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT||8080;
